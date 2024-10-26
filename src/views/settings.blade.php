4ce7c33e2ff0c30d4e097e1c091947a91b2e779b
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-26 12:07:30 +0330

    Add `data-danger` to `form-control`s.

--- src/views/settings.blade.php
+++ src/views/settings.blade.php
@@ -8,19 +8,19 @@
         <div class="card">
             <div class="card-content form">
                 <div class="form-row">
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('username')) }}">
                         <label for="username" after="{{ __('Required') }}">{{ __('Username') }}</label>
                         <input type="text" id="username" name="username" value="{{ old('username', $options->get('username')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('username')" class="mt-2" />
                     </div>
 
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('password')) }}">
                         <label for="password" after="{{ __('Required') }}">{{ __('Password') }}</label>
                         <input type="text" id="password" name="password" value="{{ old('password', $options->get('password')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('password')" class="mt-2" />
                     </div>
 
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('number')) }}">
                         <label for="number" after="{{ __('Required') }}">{{ __('Dedicated Number') }}</label>
                         <input type="text" id="number" name="number" value="{{ old('number', $options->get('number')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('number')" class="mt-2" />
@@ -32,32 +32,32 @@
         <div class="card mt-3">
             <div class="card-content form">
                 <div class="form-row">
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('verification_pattern_id')) }}">
                         <label for="verification_pattern_id" after="{{ __('Required') }}">{{ __('MelipayamakSMSGateway::attributes.verification_pattern_id') }}</label>
                         <input type="text" id="verification_pattern_id" name="verification_pattern_id" value="{{ old('verification_pattern_id', $options->get('verification_pattern_id')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('verification_pattern_id')" class="mt-2" />
                     </div>
 
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('order_paid_pattern_id')) }}">
                         <label for="order_paid_pattern_id" after="{{ __('Required') }}">{{ __('MelipayamakSMSGateway::attributes.order_paid_pattern_id') }}</label>
                         <input type="text" id="order_paid_pattern_id" name="order_paid_pattern_id" value="{{ old('order_paid_pattern_id', $options->get('order_paid_pattern_id')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('order_paid_pattern_id')" class="mt-2" />
                     </div>
     
-                    <div class="form-control">
+                    <div class="form-control" data-danger="{{ as_string($errors->has('order_shipped_pattern_id')) }}">
                         <label for="order_shipped_pattern_id" after="{{ __('Required') }}">{{ __('MelipayamakSMSGateway::attributes.order_shipped_pattern_id') }}</label>
                         <input type="text" id="order_shipped_pattern_id" name="order_shipped_pattern_id" value="{{ old('order_shipped_pattern_id', $options->get('order_shipped_pattern_id')) }}" class="default ltr-direction">
                         <x-input-error :messages="$errors->get('order_shipped_pattern_id')" class="mt-2" />
                     </div>
                 </div>
 
-                <div class="form-control">
+                <div class="form-control" data-danger="{{ as_string($errors->has('order_paid_pattern_args')) }}">
                     <label for="order_paid_pattern_args" after="{{ __('Required') }}">{{ __('MelipayamakSMSGateway::attributes.order_paid_pattern_args') }}</label>
                     <input type="text" id="order_paid_pattern_args" name="order_paid_pattern_args" value="{{ old('order_paid_pattern_args', $options->get('order_paid_pattern_args')) }}" class="default ltr-direction">
                     <x-input-error :messages="$errors->get('order_paid_pattern_args')" class="mt-2" />
                 </div>
 
-                <div class="form-control">
+                <div class="form-control" data-danger="{{ as_string($errors->has('order_shipped_pattern_args')) }}">
                     <label for="order_shipped_pattern_args" after="{{ __('Required') }}">{{ __('MelipayamakSMSGateway::attributes.order_shipped_pattern_args') }}</label>
                     <input type="text" id="order_shipped_pattern_args" name="order_shipped_pattern_args" value="{{ old('order_shipped_pattern_args', $options->get('order_shipped_pattern_args')) }}" class="default ltr-direction">
                     <x-input-error :messages="$errors->get('order_shipped_pattern_args')" class="mt-2" />

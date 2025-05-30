b0c5c196636742659be509ec49c22dd008e007e0
Hossein Zare <56504893+hossein-zare@users.noreply.github.com>
2025-05-30 12:14:13 +0330

    Update attributes.php

--- src/lang/fa/attributes.php
+++ src/lang/fa/attributes.php
@@ -3,7 +3,7 @@
 return [
     'verification_pattern_id' => 'شناسه الگوی تایید',
     'order_paid_pattern_id' => 'شناسه الگوی پرداخت سفارش',
-    'order_shipped_pattern_id' => 'شناسه الگوی ارسال سفارش',
+    'order_fulfilled_pattern_id' => 'شناسه الگوی تکمیل سفارش',
     'order_paid_pattern_args' => 'آرگومان های الگوی پرداخت سفارش',
-    'order_shipped_pattern_args' => 'آرگومان های الگوی ارسال سفارش',
+    'order_fulfilled_pattern_args' => 'آرگومان های الگوی تکمیل سفارش',
 ];

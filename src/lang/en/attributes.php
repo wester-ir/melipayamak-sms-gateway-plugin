474ce63cd7c7f50dc8550993f8859419de39b04b
Hossein Zare <56504893+hossein-zare@users.noreply.github.com>
2025-05-30 12:14:49 +0330

    Update attributes.php

--- src/lang/en/attributes.php
+++ src/lang/en/attributes.php
@@ -3,7 +3,7 @@
 return [
     'verification_pattern_id' => 'Verification Pattern ID',
     'order_paid_pattern_id' => 'Order Paid Pattern ID',
-    'order_shipped_pattern_id' => 'Order Shipped Pattern ID',
+    'order_fulfilled_pattern_id' => 'Order Fulfilled Pattern ID',
     'order_paid_pattern_args' => 'Order Paid Pattern Args',
-    'order_shipped_pattern_args' => 'Order Paid Pattern Args',
+    'order_fulfilled_pattern_args' => 'Order Fulfilled Pattern Args',
 ];
